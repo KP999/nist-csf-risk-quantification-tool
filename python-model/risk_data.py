868d5298aa5db8be5bb2a776599bcac5ff76ef9f
Keyshawn <keyshawnporter365@gmail.com>
2026-08-26 16:37:21 -0400

    Reorganize repo into excel-model and python-model folders

---
+++ python-model/risk_data.py
@@ -0,0 +1,21 @@
+risks = [
+    {
+        "name": "Data Breach",
+        "likelihood": "MEDIUM",
+        "severity": "HIGH",
+        "reductions": {
+            "Data Encryption": (0.60, 0.40),
+            "Cybersecurity Training for Employees": (0.60, 0.40),
+            "Access Control List": (0.70, 0.50),
+            "Compliance Team & Framework": (0.50, 0.60),
+            "Web Application Firewall": (0.40, 0.50),
+            "Third-Party Security Audits": (0.50, 0.70),
+        }
+    }
+    {
+        "name: Phishing"
+    }
+]
+
+if __name__ == "__main__":
+    print(risks)
